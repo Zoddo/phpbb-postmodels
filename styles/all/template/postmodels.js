5748e69fb29e5c9c1a9754b5b8cf89814f95dd64
Skouat <forum@skouat.org>
2020-01-05 18:32:15 +0100

    JSHint

--- styles/all/template/postmodels.js
+++ styles/all/template/postmodels.js
@@ -1,9 +1,8 @@
-function models_refresh(f,l)
-{
-    var url= models_refresh_url;
-    var param = 'f='+f+'&selected_language='+l;
+function models_refresh(f, l) {
+	var url = models_refresh_url;
+	var param = 'f=' + f + '&selected_language=' + l;
 
-    $.post(url, param, function(data){
-        $('#models_refresh_id').html(data);
-    });
+	$.post(url, param, function(data) {
+		$('#models_refresh_id').html(data);
+	});
 }
